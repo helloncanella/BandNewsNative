9edcb7a8dc390a3dbd3105d276e10b7bf5ef5618
Hellon Canella <helloncanella@gmail.com>
2016-12-24 16:19:18 -0200

    slider is set

--- app/tabs/audio.js
+++ app/tabs/audio.js
@@ -1,13 +1,74 @@
 import React, { Component } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Slider } from 'react-native'
 
 export class Audio extends Component {
-  render() {
-    return (
-      <View style={{}}></View>
-    );
-  }
+	constructor(){
+		super()
+		this.state = {
+			isPlaying: false,
+			currentTime: 0,
+			duration: 0
+		}
+	}
+
+	shouldComponentUpdate(nextProps, nextStates){
+		if(this.props.audio !== nextProps.audio){
+			return true
+		}	
+		return false
+	}
+
+	componentDidUpdate(){
+
+	}
+
+	setDuration(duration){
+		this.setDuration({duration})
+	}
+
+	pause(){
+		this.setState({isPlaying: false})
+	}
+
+	play(){
+		this.setState({isPlaying: true})
+	}
+
+	changeTrackPositon(newTime){
+		this.setState({currentTime: newTime})
+	}
+
+	render() {
+		const {slider} = styles		
+		return (
+			<View style={styles.audio}>
+				<Slider style={slider} minimumValue={0} maximumValue={this.duration}/>
+				<PlayPause isPlaying={this.state.isPlaying}/>
+			</View>
+		);
+	}
 }
 
-const styles = StyleSheet.create({});
+
+
+// <Streaming />
+
+class PlayPause extends Component{
+	render(){
+		return (
+			<View></View>
+		)
+	}
+}
+
+const styles = StyleSheet.create({
+	audio: {
+		flex: 1,
+		justifyContent: 'center',
+		alignSelf: 'stretch',
+	},
+	slider:{
+		flex: 1,
+	}
+});
 
